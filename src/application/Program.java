faf2627408571cd65a84e93a53b6539b9c649939
KellyCarvalho <18kellycarvalho@gmail.com>
2020-11-14 22:09:26 -0300

    Iniciando Projeto

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import BoardGame.Position;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Position pos = new Position(3,5);
+		System.out.println(pos);
+
+	}
+
+}
