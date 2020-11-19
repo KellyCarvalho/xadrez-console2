8a6e29321389c092684c6fe4ece704b4d3c43d02
KellyCarvalho <18kellycarvalho@gmail.com>
2020-11-19 18:12:00 -0300

    Adicionando movimento especial en passant completo

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -148,6 +148,31 @@ public class ChessMatch {
 		}
 		
 		
+		//movimento especial en passant
+		
+		if(p instanceof Pawn) {
+			if(source.getColumn() != target.getColumn()&& capturedPiece ==null) {
+				Position pawnPosition;
+				
+				if(p.getColor()==Color.WHITE) {
+					pawnPosition= new Position(target.getRow()+1, target.getColumn());
+				}else {
+					
+					pawnPosition= new Position(target.getRow()-1, target.getColumn());
+					
+				}
+				
+				capturedPiece = board.removePiece(pawnPosition);
+				capturedPieces.add(capturedPiece);
+				piecesOnTheBoard.remove(capturedPiece);
+			
+			}
+			
+		}
+		
+		
+		
+		
 		return capturedPiece;
 		
 	}
@@ -182,6 +207,32 @@ public class ChessMatch {
 		rook.decreaseMoveCount();
 		
 		}
+		
+		
+		//movimento especial en passant
+		
+				if(p instanceof Pawn) {
+					if(source.getColumn() != target.getColumn()&& capturedPiece ==enPassantVulnerable) {
+						ChessPiece pawn = (ChessPiece)board.removePiece(target);
+						Position pawnPosition;
+						
+						if(p.getColor()==Color.WHITE) {
+							pawnPosition= new Position(3, target.getColumn());
+						}else {
+							
+							pawnPosition= new Position(4, target.getColumn());
+							
+						}
+						
+						board.placePiece(pawn, pawnPosition);
+						
+						
+				
+					
+					}
+					
+				}
+		
 		
 	}
 	
